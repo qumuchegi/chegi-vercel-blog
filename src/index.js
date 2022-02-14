c9fd270284a4aee6c0e107b2c5b1b1bb8f37e4d5
Chege Qumu <chege.qumu@shopee.com>
2022-02-14 21:54:37 +0800

    font face optimise first paint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '@/style/index.less'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
