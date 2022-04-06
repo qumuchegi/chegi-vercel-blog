b9d675f02f7e846bd06743b3cc58d3a8d31bbdaf
Chege Qumu <chege.qumu@shopee.com>
2022-04-06 11:32:21 +0800

    rm unused tab

--- src/pages/ArticleList/index.tsx
+++ src/pages/ArticleList/index.tsx
@@ -1,47 +1,63 @@
-import SearchSelect from '@/Components/SearchSelect'
-import { useNaviToArticleDetail } from '@/routes/category/article'
-import { useStore } from '@/store'
-import { ArticleInfo } from '@/store/type'
-import { combineClassNames } from '@/utils/style'
-import React, { useRef, useState, useEffect, useMemo, useCallback, createRef } from 'react'
-import { useParams } from 'react-router-dom'
-import styles from './styles.module.less'
-import ArticlList from './list'
+import SearchSelect from "@/Components/SearchSelect";
+import { useNaviToArticleDetail } from "@/routes/category/article";
+import { useStore } from "@/store";
+import { ArticleInfo } from "@/store/type";
+import { combineClassNames } from "@/utils/style";
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  useMemo,
+  useCallback,
+  createRef,
+} from "react";
+import { useParams } from "react-router-dom";
+import styles from "./styles.module.less";
+import ArticlList from "./list";
+import Skeleton from "@/Components/Skeleton";
 
 export default function ArticleList() {
-  const params: {tabId: string} = useParams()
+  const params: { tabId: string } = useParams();
   // console.log({ params });
-  const isNarrowDevice = useStore(state => state.uiState.isNarrowDevice)
-  const getArticlesByTabName = useStore(state => state.articles.actions.getArticlesByTabName)
-  const changeSelectedTab = useStore(state => state.uiState.actions.changeSelectedTab)
-  const selectedTab = useStore(state => state.uiState.selectedTab)
-  const hadAutoSelec = useRef(false)
+  const isNarrowDevice = useStore((state) => state.uiState.isNarrowDevice);
+  const getArticlesByTabName = useStore(
+    (state) => state.articles.actions.getArticlesByTabName
+  );
+  const changeSelectedTab = useStore(
+    (state) => state.uiState.actions.changeSelectedTab
+  );
+  const selectedTab = useStore((state) => state.uiState.selectedTab);
+  const hadAutoSelec = useRef(false);
   useEffect(() => {
     if (hadAutoSelec.current) {
-      return
+      return;
     }
-    changeSelectedTab(params.tabId)
-    hadAutoSelec.current = true
-  }, [params])
-  const toArticleDetail = useNaviToArticleDetail() // { articleId: '12' }
+    changeSelectedTab(params.tabId);
+    hadAutoSelec.current = true;
+  }, [params]);
+  const toArticleDetail = useNaviToArticleDetail(); // { articleId: '12' }
 
-  const list = <ArticlList toArticleDetail={toArticleDetail}/>
+  const list = <ArticlList toArticleDetail={toArticleDetail} />;
   const articleList = useMemo(() => {
     if (selectedTab) {
-      const articles = getArticlesByTabName(selectedTab)
+      const articles = getArticlesByTabName(selectedTab);
       if (articles?.length) {
-        return list
+        return list;
       }
-      return <div className={styles.warnText}>
-        no article in this tab!
-      </div>
+      return (
+        <Skeleton
+          className={combineClassNames(styles.loadingSkeleton)}
+          showSkeleton={true}
+        />
+      );
     } else {
-      return <div>some error!</div>
+      return <div>some error!</div>;
     }
-  }, [getArticlesByTabName, selectedTab])
+  }, [getArticlesByTabName, selectedTab]);
 
-  return <div className={styles.articleList}>
-    {/* <SearchSelect options={[
+  return (
+    <div className={styles.articleList}>
+      {/* <SearchSelect options={[
       {
         key: '1',
         value: '1'
@@ -55,8 +71,7 @@ export default function ArticleList() {
         value: '3'
       }
     ]}/> */}
-    {
-      !isNarrowDevice && articleList
-    }
-  </div>
+      {!isNarrowDevice && articleList}
+    </div>
+  );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,115 +1,140 @@
-import React, { useRef, useState, useEffect, useCallback, useMemo } from 'react'
-import { useNaviToArticleDetail, useNaviToArticleList } from '@/routes/category/article'
-import styles from './style.module.less'
-import { useStore } from '@/store'
-import { combineClassNames } from '@/utils/style'
-import { repeatArr } from '@/utils/array'
-import Skeleton from '@/Components/Skeleton'
-import ArticlList from '../ArticleList/list'
-import { useBottomSheet } from '@/store/hooks'
-import icMore from '@/assets/img/more.png'
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+} from "react";
+import {
+  useNaviToArticleDetail,
+  useNaviToArticleList,
+} from "@/routes/category/article";
+import styles from "./style.module.less";
+import { useStore } from "@/store";
+import { combineClassNames } from "@/utils/style";
+import { repeatArr } from "@/utils/array";
+import Skeleton from "@/Components/Skeleton";
+import ArticlList from "../ArticleList/list";
+import { useBottomSheet } from "@/store/hooks";
+import icMore from "@/assets/img/more.png";
 
 export default function Home() {
-  const [isMoreIconVisible, setIsMoreIconVisible] = useState(false)
-  const headerRef = useRef<any>()
-  const [showArticleTitle, setShowArticleTitle] = useState(false)
-  const toArticleList = useNaviToArticleList()
-  const tabArticles = useStore(state => state.articles.tabArticles)
-  const changeSelectedTab = useStore(state => state.uiState.actions.changeSelectedTab)
-  const changeSelectedArticle = useStore(state => state.uiState.actions.changeSelectedArticle)
-  const selectedTab = useStore(state => state.uiState.selectedTab)
-  const {
-    open: openBottomSeet,
-    close: closeBottomSheet
-   } = useBottomSheet()
-  const tabNames = useMemo(() => tabArticles ? Object.keys(tabArticles) : [], [tabArticles])
-  const toArticleDetail = useNaviToArticleDetail() // { articleId: '12' }
-  const selectedArticleId = useStore(state => state.uiState.selectedArticleId)
-  const getArticleInfo = useStore(state => state.articles.actions.getArticleInfo)
+  const [isMoreIconVisible, setIsMoreIconVisible] = useState(false);
+  const headerRef = useRef<any>();
+  const [showArticleTitle, setShowArticleTitle] = useState(false);
+  const toArticleList = useNaviToArticleList();
+  const tabArticles = useStore((state) => state.articles.tabArticles);
+  const changeSelectedTab = useStore(
+    (state) => state.uiState.actions.changeSelectedTab
+  );
+  const changeSelectedArticle = useStore(
+    (state) => state.uiState.actions.changeSelectedArticle
+  );
+  const selectedTab = useStore((state) => state.uiState.selectedTab);
+  const { open: openBottomSeet, close: closeBottomSheet } = useBottomSheet();
+  const tabNames = useMemo(
+    () => (tabArticles ? Object.keys(tabArticles) : []),
+    [tabArticles]
+  );
+  const toArticleDetail = useNaviToArticleDetail(); // { articleId: '12' }
+  const selectedArticleId = useStore(
+    (state) => state.uiState.selectedArticleId
+  );
+  const getArticleInfo = useStore(
+    (state) => state.articles.actions.getArticleInfo
+  );
   const articleTitle = useMemo(() => {
-    return getArticleInfo(selectedArticleId)?.title
-  }, [getArticleInfo,selectedArticleId ])
-  const articleList = <div>
-    <ArticlList  toArticleDetail={toArticleDetail}/>
-  </div>
-  const onPressTab = useCallback((tabName) => {
-    if (tabName === selectedTab) {
-
-    } else {
-      changeSelectedTab(tabName)
-      toArticleList({ tabId: tabName })
-      changeSelectedArticle(null)
-    }
-    openBottomSeet(articleList, <h2>{tabName}</h2>)
-  }, [toArticleList, changeSelectedTab, changeSelectedArticle, selectedTab, openBottomSeet])
+    return getArticleInfo(selectedArticleId)?.title;
+  }, [getArticleInfo, selectedArticleId]);
+  const articleList = (
+    <div>
+      <ArticlList toArticleDetail={toArticleDetail} />
+    </div>
+  );
+  const onPressTab = useCallback(
+    (tabName) => {
+      if (tabName === selectedTab) {
+      } else {
+        changeSelectedTab(tabName);
+        toArticleList({ tabId: tabName });
+        changeSelectedArticle(null);
+      }
+      openBottomSeet(articleList, <h2>{tabName}</h2>);
+    },
+    [
+      toArticleList,
+      changeSelectedTab,
+      changeSelectedArticle,
+      selectedTab,
+      openBottomSeet,
+    ]
+  );
 
   const tabs = useMemo(() => {
     if (tabNames.length) {
-      return tabNames.map(
-        tabName => <div
+      return tabNames.map((tabName) => (
+        <div
           key={tabName}
-          className={
-            combineClassNames(
-              styles.tabItem,
-              tabName === selectedTab ? styles.selectedtabItem : ''
-            )
-          }
+          className={combineClassNames(
+            styles.tabItem,
+            tabName === selectedTab ? styles.selectedtabItem : ""
+          )}
           onClick={() => onPressTab(tabName)}
         >
           {tabName}
         </div>
-      )
+      ));
     } else {
       return repeatArr(
-        <Skeleton showSkeleton className={styles.tabSkeleton}/>,
+        <Skeleton showSkeleton className={styles.tabSkeleton} />,
         4,
         (_, index) => <div key={index}>{_}</div>
-      )
+      );
     }
-  }, [tabNames, selectedTab, onPressTab])
+  }, [tabNames, selectedTab, onPressTab]);
 
-  const lastScrollTop = useRef(0)
+  const lastScrollTop = useRef(0);
   const handleScroll = useCallback(() => {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (scrollTop < lastScrollTop.current || scrollTop === 0) {
       // down
-      setShowArticleTitle(false)
+      setShowArticleTitle(false);
     } else {
       // up
-      setShowArticleTitle(true)
+      setShowArticleTitle(true);
     }
-    lastScrollTop.current = scrollTop
-  }, [])
+    lastScrollTop.current = scrollTop;
+  }, []);
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [handleScroll])
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
   useEffect(() => {
-    setIsMoreIconVisible(headerRef.current?.scrollWidth > headerRef.current?.clientWidth)
-  }, [])
-  return <div className={styles.container} ref={headerRef}>
-    <div className={
-      combineClassNames(
-        styles.tabContainer,
-        showArticleTitle 
-        ? styles.up
-        : styles.down
-      )
-    }>
-      {
-        tabs
-      }
-      {/* <img src={icMore} className={styles.moreIcon}/> */}
+    setIsMoreIconVisible(
+      headerRef.current?.scrollWidth > headerRef.current?.clientWidth
+    );
+  }, []);
+  return (
+    <div className={styles.container} ref={headerRef}>
+      <div
+        className={combineClassNames(
+          styles.tabContainer,
+          showArticleTitle ? styles.up : styles.down
+        )}
+      >
+        {tabs}
+        {/* <img src={icMore} className={styles.moreIcon}/> */}
+      </div>
+      <div
+        className={combineClassNames(
+          styles.articleTitle,
+          showArticleTitle ? styles.up : styles.down
+        )}
+      >
+        {articleTitle}
+      </div>
     </div>
-    <div className={
-      combineClassNames(
-        styles.articleTitle,
-        showArticleTitle 
-        ? styles.up
-        : styles.down
-      )
-    }>{articleTitle}</div>
-  </div>
+  );
 }
